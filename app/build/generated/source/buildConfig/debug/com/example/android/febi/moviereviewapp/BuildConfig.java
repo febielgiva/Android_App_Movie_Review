a03334c04fcf49e4a2d43759ee11ff7053c7be60
febielgiva <idoffebi@gmail.com>
2016-05-10 18:11:55 -0700

    initial commit of app -1st milestone

---
+++ app/build/generated/source/buildConfig/debug/com/example/android/febi/moviereviewapp/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.android.febi.moviereviewapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.android.febi.moviereviewapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: debug
+  public static final String MOVIE_REVIEW_API_KEY = "05a10b04fb4e38de298469c817e03540:2:75104766";
+}
